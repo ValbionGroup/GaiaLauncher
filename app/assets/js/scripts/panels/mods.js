ff1a422e4f05bb7d89659045afa64a3501bf8a11
Luc'HACK <espiet.l@valbion.com>
2023-10-29 19:23:09 +0100

    🎉 Add mod selection setting

--- app/assets/js/scripts/panels/mods.js
+++ app/assets/js/scripts/panels/mods.js
@@ -13,12 +13,152 @@ async function populateServerIndicator() {
 
 	document.querySelector("#panels #mods .server-box").innerHTML = `
 			<div class="header">
-				<div class="icon"><img src="${serv.rawServer.icon}" /></div>
+				<div class="icon"><img src="${serv.rawServer.launcherPage.icon}" /></div>
 				<div class="title">${serv.rawServer.name}</div>
 				<div class="description">${serv.rawServer.description}</div>
 				<div class="switch icon-switchServer"></div>
 			</div>
 		`
+}
+
+/**
+ * Recursively build the mod UI elements.
+ * 
+ * @param {Object[]} mdls An array of modules to parse.
+ * @param {boolean} submodules Whether or not we are parsing submodules.
+ * @param {Object} servConf The server configuration object for this module level.
+ */
+function parseModulesForUI(mdls, submodules, servConf){
+
+    let reqMods = ''
+    let optMods = ''
+
+    for(const mdl of mdls){
+
+        if(mdl.rawModule.type === Type.ForgeMod || mdl.rawModule.type === Type.LiteMod || mdl.rawModule.type === Type.LiteLoader){
+
+            if(mdl.getRequired().value){
+
+                reqMods += `<div id="${mdl.getVersionlessMavenIdentifier()}" class="base-mod settings-${submodules ? 'sub' : ''}mod" enabled>
+                    <div class="mod-content">
+                        <div class="main-wrapper">
+                            <div class="status active"></div>
+                            <div class="details">
+                                <span class="name">${mdl.rawModule.name}</span>
+                                <span class="version">v${mdl.mavenComponents.version}</span>
+                            </div>
+                        </div>
+                        <label class="mod-switch" reqmod>
+                            <input type="checkbox" checked>
+                            <span class="switch-slider"></span>
+                        </label>
+                    </div>
+                    ${mdl.subModules.length > 0 ? `<div class="base-mod">
+                        ${Object.values(parseModulesForUI(mdl.subModules, true, servConf[mdl.getVersionlessMavenIdentifier()])).join('')}
+                    </div>` : ''}
+                </div>`
+
+            } else {
+
+                const conf = servConf[mdl.getVersionlessMavenIdentifier()]
+                const val = typeof conf === 'object' ? conf.value : conf
+
+                optMods += `<div id="${mdl.getVersionlessMavenIdentifier()}" class="base-mod settings-${submodules ? 'sub' : ''}mod" ${val ? 'enabled' : ''}>
+                    <div class="mod-content">
+                        <div class="main-wrapper">
+                            <div class="status ${val ? 'active' : ''}"></div>
+                            <div class="details">
+                                <span class="name">${mdl.rawModule.name}</span>
+                                <span class="version">v${mdl.mavenComponents.version}</span>
+                            </div>
+                        </div>
+                        <label class="mod-switch">
+                            <input type="checkbox" formod="${mdl.getVersionlessMavenIdentifier()}" ${val ? 'checked' : ''}>
+                            <span class="switch-slider"></span>
+                        </label>
+                    </div>
+                    ${mdl.subModules.length > 0 ? `<div class="base-mod">
+                        ${Object.values(parseModulesForUI(mdl.subModules, true, conf.mods)).join('')}
+                    </div>` : ''}
+                </div>`
+
+            }
+        }
+    }
+
+    return {
+        reqMods,
+        optMods
+    }
+
+}
+
+/**
+ * Bind functionality to mod config toggle switches. Switching the value
+ * will also switch the status color on the left of the mod UI.
+ */
+function bindModsToggleSwitch(){
+    const sEls = document.querySelectorAll('[formod]')
+    Array.from(sEls).map((v, index, arr) => {
+        v.onchange = () => {
+            if(v.checked) {
+                document.getElementById(v.getAttribute('formod')).setAttribute('enabled', '')
+				document.getElementById(v.getAttribute('formod')).querySelector('.status').classList.add('active')
+            } else {
+                document.getElementById(v.getAttribute('formod')).removeAttribute('enabled')
+				document.getElementById(v.getAttribute('formod')).querySelector('.status').classList.remove('active')
+            }
+        }
+    })
+}
+
+
+/**
+ * Save the mod configuration based on the UI values.
+ */
+function saveModConfiguration(){
+    const serv = ConfigManager.getSelectedServer()
+    const modConf = ConfigManager.getModConfiguration(serv)
+    modConf.mods = _saveModConfiguration(modConf.mods)
+    ConfigManager.setModConfiguration(serv, modConf)
+}
+
+/**
+ * Recursively save mod config with submods.
+ * 
+ * @param {Object} modConf Mod config object to save.
+ */
+function _saveModConfiguration(modConf){
+    for(let m of Object.entries(modConf)){
+        const tSwitch = settingsModsContainer.querySelectorAll(`[formod='${m[0]}']`)
+        if(!tSwitch[0].hasAttribute('dropin')){
+            if(typeof m[1] === 'boolean'){
+                modConf[m[0]] = tSwitch[0].checked
+            } else {
+                if(m[1] != null){
+                    if(tSwitch.length > 0){
+                        modConf[m[0]].value = tSwitch[0].checked
+                    }
+                    modConf[m[0]].mods = _saveModConfiguration(modConf[m[0]].mods)
+                }
+            }
+        }
+    }
+    return modConf
+}
+
+/**
+ * Resolve and update the mods on the UI.
+ */
+async function resolveModsForUI(){
+    const serv = (await DistroAPI.getDistribution()).getServerById(ConfigManager.getSelectedServer())
+
+    const servConf = ConfigManager.getModConfiguration(ConfigManager.getSelectedServer())
+
+    const modStr = parseModulesForUI(serv.modules, false, servConf.mods)
+
+    document.getElementById('listModsReq').innerHTML = modStr.reqMods
+    document.getElementById('listModsOpt').innerHTML = modStr.optMods
 }
 
 /* RAM SETTINGS */
@@ -36,6 +176,9 @@ function populateMemoryStatus(){
 async function refreshModsPanel() {
 	await populateServerIndicator()
 	populateMemoryStatus()
+	await resolveModsForUI()
+    // await resolveShaderpacksForUI()
+    bindModsToggleSwitch()
 }
 
 refreshModsPanel()
